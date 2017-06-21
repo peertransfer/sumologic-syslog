7adfe50879cce07fc35bd668e131d309241e250c
Jose Luis Salas <josacar@users.noreply.github.com>
2017-06-21 17:14:41 +0200

    Add TLS support

--- spec/resources/rsyslog/create_spec.rb
+++ spec/resources/rsyslog/create_spec.rb
@@ -2,26 +2,33 @@ require_relative '../../spec_helper'
 
 describe 'sumologic-syslog-test::rsyslog-create' do
   cached(:subject) do
-    runner = ChefSpec::SoloRunner.new(step_into: %w[sumologic_syslog_rsyslog])
+    runner = ChefSpec::SoloRunner.new(step_into: 'sumologic_syslog_rsyslog')
     runner.converge(described_recipe)
   end
   let(:node) { subject.node }
+  let(:rsyslog_config) { '/etc/rsyslog.d/sumologic.conf' }
+  let(:sumologic_cert) { '/etc/syslog.sumologic.crt' }
 
   it { is_expected.to include_recipe('rsyslog::default') }
 
   it { is_expected.to install_package('rsyslog-gnutls') }
 
   it 'downloads required CA' do
-    is_expected.to create_remote_file('/etc/syslog.sumologic.crt').
+    is_expected.to create_remote_file(sumologic_cert).
       with(source: 'https://www.geotrust.com/resources/root_certificates/certificates/GeoTrust_Primary_CA.pem')
   end
 
   it 'renders rsyslog config file' do
-    is_expected.to render_file('/etc/rsyslog.d/sumologic.conf').
+    is_expected.to render_file(rsyslog_config).
       with_content('syslog.collection.eu.sumologic.com')
-    is_expected.to render_file('/etc/rsyslog.d/sumologic.conf').
+
+    is_expected.to render_file(rsyslog_config).
       with_content('6514')
-    is_expected.to render_file('/etc/rsyslog.d/sumologic.conf').
+
+    is_expected.to render_file(rsyslog_config).
       with_content('MagicToken@41123')
+
+    is_expected.to render_file(rsyslog_config).
+      with_content("$DefaultNetstreamDriverCAFile #{sumologic_cert}")
   end
 end
