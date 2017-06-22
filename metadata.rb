37a4c3bd81984e9d55ba975761416868e54e9217
Jose Luis Salas <josacar@users.noreply.github.com>
2017-06-22 14:35:33 +0200

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@flywire.com'
 license          'MIT'
 description      'Installs/Configures sumologic to send logs via syslog'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 issues_url 'https://github.com/peertransfer/sumologic-syslog/issues'
 source_url 'https://github.com/peertransfer/sumologic-syslog'
